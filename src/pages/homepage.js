bcb979fc89d23c3d50621d0220f6487bb7802eab
Mai Doan <neko_hime1212@yahoo.com>
2021-10-14 20:26:26 +0700

    Add galaxy background

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -6,6 +6,10 @@ import '../styles/modules/_homepage.scss';
 export default function Homepage() {
    return (
       <div class="bg-galaxy full-height">
+         <div id="stars"></div>
+         <div id="stars2"></div>
+         <div id="stars3"></div>
+        
          <div class="page-wrapper homepage-wrapper">
             <div class="homepage__contents">
                <HeadingText subtitle="Tell me how you are feeling" title="Calm down with us. Throw away your troubles, double your joy." />
