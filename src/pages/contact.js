82f6e845c426788755e9ac29ce2ea26e163e4925
Mai Doan <neko_hime1212@yahoo.com>
2021-10-16 22:16:35 +0700

    feat: create contact form

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import GalaxyBackground from '../components/GalaxyBackground';
+import HeadingText from '../components/HeadingText';
+import '../styles/modules/_contact.scss';
+
+export default function ContactUs() {
+   return (
+      <Layout>
+         <GalaxyBackground>
+            <div class="page-wrapper contact-wrapper">
+               <HeadingText subtitle="We are pleased to hear from you" title="Send us your message" />
+               <div>
+                  <form class="contact-form">
+                     <input class="contact__mail" name="contact[email]" type="email" placeholder="Email (optional)" />
+                     <textarea class="contact__message" rows="10" name="contact[message]" placeholder="Your message..." />
+                  </form>
+               </div>
+            </div>
+         </GalaxyBackground>
+      </Layout>
+   )
+}
