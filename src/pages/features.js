b97aadc7c271ec8c58b2e597562a032e8a4edf4a
Mai Doan <neko_hime1212@yahoo.com>
2021-10-16 12:00:49 +0700

    refactor: create background component

--- src/pages/features.js
+++ src/pages/features.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import CircleButton from '../components/CircleButton';
+import GalaxyBackground from '../components/GalaxyBackground';
 import HeadingText from '../components/HeadingText';
 import '../styles/modules/_features.scss';
-export default function Features() {
+
+function IncomingFeature() {
   return (
-    <div class="bg-galaxy full-height">
-      <div id="stars"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
-      <div className="page-wrapper features-container">
-        <div className="features-contents  animation-fade-up">
-          <HeadingText subtitle="Stay tune" title="We will launch soon" textAlign="center" />
-          <div className="features-circle-wrap">
-            <CircleButton href="/aboutUs" buttonText="Take a tour" />
-          </div>
+    <div className="page-wrapper features-container">
+      <div className="features-contents  animation-fade-up">
+        <HeadingText subtitle="Stay tune" title="We will launch soon" textAlign="center" />
+        <div className="features-circle-wrap">
+          <CircleButton href="/aboutUs" buttonText="Take a tour" />
         </div>
       </div>
     </div>
+  )
+}
+
+export default function Features() {
+  return (
+    <GalaxyBackground children={<IncomingFeature />} />
   );
 }
