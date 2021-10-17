ec379191f28e4b5505fea978be74e57b89256d03
PhuongPhg <thaophuong.trinh.9@gmail.com>
2021-10-17 21:57:07 +0700

    revert: homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -8,17 +8,14 @@ import '../styles/modules/_homepage.scss';
 export default function Homepage() {
   return (
     <Layout>
-      <GalaxyBackground>
-        <div class="page-wrapper homepage-wrapper">
-          <div class="homepage__contents animation-fade-up">
-            <HeadingText
-              subtitle="Tell me how you are feeling"
-              title="Calm down with us. Throw away your troubles, double your joy."
-            />
-            <CircleButton buttonText="Start now" href="/features" />
+       <GalaxyBackground>
+          <div class="page-wrapper homepage-wrapper">
+             <div class="homepage__contents animation-fade-up">
+                <HeadingText subtitle="Tell me how you are feeling" title="Calm down with us. Throw away your troubles, double your joy." />
+                <CircleButton buttonText="Start now"  href="/features"/>
+             </div>
           </div>
-        </div>
-      </GalaxyBackground>
+       </GalaxyBackground>
     </Layout>
-  );
+ )
 }
