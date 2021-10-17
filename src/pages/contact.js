4919d136fb311b076635d37a000abbb6c96309d0
Mai Doan <neko_hime1212@yahoo.com>
2021-10-17 11:58:51 +0700

    feat: add loader when submit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,16 +4,19 @@ import GalaxyBackground from '../components/GalaxyBackground';
 import HeadingText from '../components/HeadingText';
 import emailjs, { init } from 'emailjs-com';
 import '../styles/modules/_contact.scss';
+import Loader from '../components/Loader';
 
 export default function ContactUs() {
    const mailInput = useRef();
    const messageInput = useRef();
    const [formResponse, setFormResponse] = useState('');
    const [formStatus, setFormStatus] = useState('');
+   const [loading, setLoading] = useState(false);
 
    const onSubmit = (e) => {
       e.preventDefault();
-      
+      setLoading(true);
+
       init(process.env.REACT_APP_EMAILJS_UID);
       const serviceID = process.env.REACT_APP_EMAILJS_SID;
       const templateID = process.env.REACT_APP_EMAILJS_TID;
@@ -27,9 +30,11 @@ export default function ContactUs() {
          setFormResponse('Thank you for your reaching out!');
          setFormStatus('success');
          resetForm();
+         setLoading(false);
       }, (err) => {
          setFormResponse("Failed to send message. Try again!");
          setFormStatus('error');
+         setLoading(false);
          console.log(JSON.stringify(err));
       });
    }
@@ -47,10 +52,11 @@ export default function ContactUs() {
                <div>
                   <form className="contact-form" onSubmit={(e) => onSubmit(e)}>
                      { formStatus && <div className={`contact__response contact__response--${formStatus}`}>{ formResponse }</div>}
-                      <input className="contact__mail" type="email" placeholder="Email (optional)" ref={mailInput} />
-                     <textarea className="contact__message" rows="10" name="contact[message]" placeholder="Your message..."
-                        ref={messageInput} />
-                     <button className="primary-btn contact__submit" type="submit">Send</button>
+                      <input className="contact__mail" name="contact[email]" type="email" placeholder="Email (optional)" ref={mailInput} />
+                     <textarea className="contact__message" rows="10" name="contact[message]" placeholder="Your message..." ref={messageInput} required />
+                     <button className="primary-btn contact__submit" type="submit">
+                        { loading ? <Loader color="#5A5F63" size="25px" borderWidth="4px" /> : "Send" }
+                     </button>
                   </form>
                </div>
             </div>
